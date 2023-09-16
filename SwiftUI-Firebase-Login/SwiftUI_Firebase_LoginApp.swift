8ef253a36c42da8c560844333579b1f690d7675d
phong070 <tpcreative.co@gmail.com>
2023-09-16 12:15:42 +0700

    Initial Commit

---
+++ SwiftUI-Firebase-Login/SwiftUI_Firebase_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Firebase_LoginApp.swift
+//  SwiftUI-Firebase-Login
+//
+//  Created by phong070 on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Firebase_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
